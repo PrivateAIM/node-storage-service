abe32c95bab32d5ae8ca415a95c771c4edb1f1da
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-02-12 12:04:19 +0100

    feat: add an entrypoint to generate the openapi schema file

--- project/main.py
+++ project/main.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 import uvicorn
 
 from project.server import get_server_instance
@@ -7,6 +10,16 @@ app = get_server_instance()
 
 def run_server():
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+def openapi_spec(filename: str = "openapi.json"):
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        pass
+    spec = app.openapi()
+    with open(filename, "w") as f:
+        json.dump(spec, f)
 
 
 if __name__ == "__main__":
