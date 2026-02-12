a28829d06f4fdafac39195eee648c9db5ad15b18
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-02-12 12:51:48 +0100

    chore: harden spectral bash script

--- spectral/spectral.sh
+++ spectral/spectral.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
 
 # This script is used by a pre-commit to lint the OpenAPI specification of this service with Spectral.
 openapi-spec spectral/openapi.json
-docker run --rm -v $(pwd):/tmp stoplight/spectral lint --ruleset "/tmp/.spectral.yaml" "/tmp/spectral/openapi.json" $@
+docker run --rm -v "$(pwd)":/tmp stoplight/spectral lint --ruleset "/tmp/.spectral.yaml" "/tmp/spectral/openapi.json" "$@"
