6c7427d7765800972bd86c2429c857a186489058
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-02-12 12:07:29 +0100

    chore: add a pre commit hook for spectral

---
+++ spectral/spectral.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is used by a pre-commit to lint the OpenAPI specification of this service with Spectral.
+openapi-spec spectral/openapi.json
+docker run --rm -v $(pwd):/tmp stoplight/spectral lint --ruleset "/tmp/.spectral.yaml" "/tmp/spectral/openapi.json" $@
