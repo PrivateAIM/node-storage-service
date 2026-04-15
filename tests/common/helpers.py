e631022cb06f8eea640d22a94782a43c41a64bbe
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-04-15 11:46:01 +0200

    test: use ec keys with bit length of 256 to align with the Hub

--- tests/common/helpers.py
+++ tests/common/helpers.py
@@ -54,8 +54,8 @@ def next_random_bytes(rng: random.Random, n: int = 16):
     return rng.randbytes(n)
 
 
-def next_ecdh_keypair():
-    private_key = ec.generate_private_key(curve=ec.SECP384R1())
+def next_ecdh_keypair(curve=ec.SECP256R1()):
+    private_key = ec.generate_private_key(curve=curve)
     public_key = private_key.public_key()
 
     return private_key, public_key
