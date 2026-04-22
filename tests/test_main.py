460d5c5c649fb2b54b3ed8733fe8268675adbc65
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-04-22 13:21:09 +0200

    test(perf): let pytest handle cleanup of temporary file

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,14 +6,12 @@ from project.main import openapi_spec
 from tests.common.helpers import next_random_string
 
 
-def test_openapi_spec(monkeypatch):
-    filename = f"{next_random_string()}.json"
+def test_openapi_spec(monkeypatch, tmp_path):
+    filename = f"{tmp_path}/{next_random_string()}.json"
     monkeypatch.setattr("sys.argv", [None, filename])
     openapi_spec()
 
     assert pathlib.Path(filename).exists()
-
-    pathlib.Path(filename).unlink()
 
 
 def test_openapi_spec_wrong_file(monkeypatch):
