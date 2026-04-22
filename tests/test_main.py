49d0bb7f6005a0c80aed070960495f5547c84509
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-04-22 17:18:48 +0200

    test: start uvicorn server

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,13 @@
 import pathlib
+import threading
 
+import httpx
+import peewee as pw
 import pytest
 
-from project.main import openapi_spec
-from tests.common.helpers import next_random_string
+from project import crud
+from project.main import openapi_spec, config_server
+from tests.common.helpers import next_random_string, eventually
 
 
 def test_openapi_spec(monkeypatch, tmp_path):
@@ -22,3 +26,29 @@ def test_openapi_spec_wrong_file(monkeypatch):
         openapi_spec()
 
     assert str(e.value) == f"File {filename} needs to be a json file."
+
+
+def test_run_server(monkeypatch):
+    host, port = "127.0.0.1", 8001
+    server = config_server(host=host, port=port)
+
+    # Reset proxy in case it was already initialized.
+    monkeypatch.setattr(crud, "proxy", pw.DatabaseProxy())
+
+    thread = threading.Thread(target=server.run, daemon=True)
+    thread.start()
+
+    def _wait_for_server_to_start():
+        return server.started
+
+    assert eventually(_wait_for_server_to_start)
+
+    r = httpx.get(f"http://{host}:{port}/healthz")
+
+    assert r.status_code == 200
+    assert r.json() == {"status": "ok"}
+
+    server.should_exit = True
+    thread.join(timeout=5)
+
+    assert not thread.is_alive()
