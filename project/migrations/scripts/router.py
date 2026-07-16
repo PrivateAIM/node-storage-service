41c3dc7554c88a85c17574a37f87af0d36af9cf2
pbrassel <52356233+pbrassel@users.noreply.github.com>
2026-07-16 12:05:14 +0200

    fix: create `logs` directory if it does not exist

--- project/migrations/scripts/router.py
+++ project/migrations/scripts/router.py
@@ -1,4 +1,5 @@
 import json
+import os
 import logging.config
 
 from peewee_migrate import Router
@@ -8,6 +9,8 @@ from project.server import get_project_root
 
 
 def init_router() -> Router:
+    os.makedirs(get_project_root() / "logs", exist_ok=True)
+
     # peewee-migrate's logger has no handler configured per default.
     with open(get_project_root() / "config" / "logging.json") as f:
         config = json.load(f)
